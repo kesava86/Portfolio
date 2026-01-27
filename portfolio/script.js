26e43fa07039307330bae63712d7b596c6a15b2f
kesava86 <kkesava493@gmail.com>
2026-01-27 17:49:20 +0530

    inital commit

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,6 +1,3 @@
-const hamburger = document.querySelector(".hamburger");
-const menu_bar = document.querySelector(".navbar-menu .menu-bar");
-
-hamburger.addEventListener("click", () => {
-    menu_bar.classList.toggle("show");
-});
+function toggleMenu() {
+    document.getElementById("navLinks").classList.toggle("show");
+}
