c15afd449978864210b04ff53f1cce81b14580d6
kesava86 <kkesava493@gmail.com>
2026-01-29 11:34:09 +0530

    hamburger

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function showMessage() {
   alert("Thanks for visiting my portfolio!");
 }
+
+
+const toggler = document.getElementById("menuToggleBtn");
+  const icon = document.getElementById("menuIcon");
+
+  toggler.addEventListener("click", () => {
+    if (icon.classList.contains("bi-list")) {
+      icon.classList.remove("bi-list");
+      icon.classList.add("bi-x");
+    } else {
+      icon.classList.remove("bi-x");
+      icon.classList.add("bi-list");
+    }
+  });
